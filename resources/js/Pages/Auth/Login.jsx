81ab054dc94e379e3fec257b77085acf1a65aa4e
= <muhbinrang650@gmail.com>
2023-02-13 23:08:34 +0800

    Sedikit perubahan lagi

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -5,7 +5,7 @@ import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { Head, useForm } from '@inertiajs/react';
 import swal from 'sweetalert';
 
 export default function Login({ status, canResetPassword }) {
