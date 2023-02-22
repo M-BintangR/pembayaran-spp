a4bad07d4df64f9e3441ecf2d55a56dd027cb79e
= <muhbinrang650@gmail.com>
2023-02-23 07:51:47 +0800

    Repair footer

--- resources/js/Layouts/Footer.jsx
+++ resources/js/Layouts/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='w-full bg-gray-100 shadow-2xl md:fixed bottom-0 -z-40 md:text-sm text-xs text-gray-500'>
+        <div className='w-full bg-gray-100 shadow-2xl md:fixed bottom-0 md:text-sm text-xs text-gray-500'>
             <p className='float-right p-1 px-2'>@copyright 2023 by bintang</p>
         </div>
     )
